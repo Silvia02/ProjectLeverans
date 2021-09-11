6550c147e26b319c4f562d6c590b30427ecfdecb
Viviann Rydh <rydh.vivian@gmail.com>
2021-09-11 15:45:53 +0200

    Added State 'Favorites' and Favorites route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,14 +9,33 @@ import './App.css'
 import FrontPage from './pages/FrontPage';
 import Products from './pages/Home';
 import Login from './pages/Login';
-
 import Product from './pages/Product';
 import MyCart from './pages/MyCart';
 import RegisterPage from './pages/RegisterPage';
 import ShippingDetails from './pages/ShippingDetails';
+import Favorites from './pages/Favorites'
 
 function App() {
-  //hellow world
+  const [favorites, setFavorites] = useState([])
+  
+    const onAdd = (product) => {
+      //const exist = favorites.find(favorite => favorite._id === product._id);
+      setFavorites([...favorites, product])
+      
+      {/*if (exist) {
+        setFavorites(
+          favorites.map(favorite =>
+            favorite._id === product._id ? { ...exist, quantity: exist.quantity + 1 }
+              : favorite
+          )
+        )
+      } else {
+        setFavorites([...favorites, product])
+      }*/}
+      console.log('favorite button is onclick')
+      console.log(product)
+      console.log(favorites)
+    }
   return (
      <div className="App">
         <Router>
@@ -31,16 +50,28 @@ function App() {
               <RegisterPage />
             </Route>
             <Route exact path="/products">
-              <Products />
+              <Products 
+                onAdd={onAdd}
+                favorites={favorites}
+                />
             </Route>
-          <Route path="/products/:id">
-            <Product />
-          </Route>
+            <Route path="/products/:id">
+              <Product
+                favorites={favorites}
+                onAdd={onAdd} 
+            />
+            </Route>
             <Route path = "/checkout">
-              <ShippingDetails/>
-          </Route>
-          <Route path = "/mycart">
-              <MyCart/>
+                <ShippingDetails/>
+            </Route>
+            <Route path = "/mycart">
+                <MyCart/>
+            </Route>
+            <Route path="/favorites" >
+              <Favorites
+                favorites={favorites}
+                onAdd={onAdd}
+                />
             </Route>
           </Switch>
         </Router>
