07c36e85fd44b3f3f0b38801b5c9b01f9faaea0a
nisha saraswat <nisha0217@gmail.com>
2021-09-11 21:33:29 +0200

    Login user done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React ,{useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useHistory } from "react-router";
 import {
   StlyedFormWrappper,
   StlyedForm,
@@ -8,23 +10,46 @@ import {
 } from "./FrontPageStyle";
 
 
-const Login = () => {
+const Login = ({setUserLogin}) => {
+
+  const history = useHistory();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const {
+      name,
+      value
+    } = e.target;
+    setUser({
+      ...user,
+      [name]: value
+    })
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:4000/login', user)
+      .then(res => {
+        alert(res.data.message)
+        setUserLogin(res.data.user)
+        history.push('/home')
+      })
+  }
   return (
     <>
       <h1>Login Page</h1>
       <StlyedFormWrappper>
         <StlyedForm>
-          <label htmlFor="UserName">User Name</label>
-          <StlyedInput type="text" name="UserName" placeholder="UserName" />
+          <label htmlFor="Email">Email</label>
+          <StlyedInput type="email" name="email" value={user.email} placeholder="UserName" onChange={handleChange}/>
           <label htmlFor="Password">Password</label>
-          <StlyedInput type="text" name="Password" placeholder="Password" />
-          <Link to="/">
-            <Button>Login</Button>
-          </Link>
-
-          <Link to="/register">
-            <Button>Register</Button>
-          </Link>
+          <StlyedInput type="password" name="password" value={user.password} placeholder="Password" onChange={handleChange} />
+            <Button onClick={handleLogin}>Login</Button>
+            <Button onClick={()=> history.push('/register')}>Register</Button>
+          
         </StlyedForm>
       </StlyedFormWrappper>
     </>
