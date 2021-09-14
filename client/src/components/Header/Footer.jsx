386fe70cde532a707b36bae284af95b42d112a0f
Viviann Rydh <rydh.vivian@gmail.com>
2021-09-14 15:40:07 +0200

    add footer

---
+++ client/src/components/Header/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../images/shop.png'
+
+const Footer = () => {
+  return (
+    <div>
+      <img src={logo} alt="logo"/>
+    </div>
+  )
+}
+
+export default Footer
