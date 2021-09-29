7e1840fcabcf37c43c9c5c11179fb75032054fe5
nisha saraswat <nisha0217@gmail.com>
2021-09-30 00:00:09 +0200

    error message color changed

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -90,7 +90,7 @@ const Register = () => {
             <div
               style={{ marginTop: "15px", padding: "5px", textAlign: "center" }}
             >
-              <p style={{ color: "orangered", fontSize: "20px" }}>
+              <p style={{ color: "white", fontSize: "20px" }}>
                 {errorMessage}
               </p>
             </div>
